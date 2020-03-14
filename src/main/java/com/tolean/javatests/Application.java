9ffd78b84b9655eb4775beff9ebc1cd625f9d06e
Tomek <tomasz.kolodziej@poczta.pl>
2020-03-14 14:17:35 +0100

    log 1

--- src/main/java/com/tolean/javatests/Application.java
+++ src/main/java/com/tolean/javatests/Application.java
@@ -7,6 +7,7 @@ public class Application {
     public static void main(String[] args) {
         CompletableFutureLearning completableFutureLearning = new CompletableFutureLearning();
         completableFutureLearning.run();
+        // log1
     }
 
 }
