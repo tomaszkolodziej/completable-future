7a704292ec7027178000b5d2c22f135185971481
Tomek <tomasz.kolodziej@poczta.pl>
2020-03-14 14:31:29 +0100

    log 3

--- src/main/java/com/tolean/javatests/completablefuture/CompletableFutureLearning.java
+++ src/main/java/com/tolean/javatests/completablefuture/CompletableFutureLearning.java
@@ -3,7 +3,7 @@ package com.tolean.javatests.completablefuture;
 public class CompletableFutureLearning {
 
     public void run() {
-        System.out.println("completable future learning");
+        System.out.println("Completable future learning");
     }
 
 }
