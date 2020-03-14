80310ca0ec910abfef17288ab68d1705e0d6b07d
Tomek <tomasz.kolodziej@poczta.pl>
2020-03-14 14:15:26 +0100

    completable future
    log 2
    
    log 3

---
+++ src/main/java/com/tolean/javatests/Application.java
@@ -0,0 +1,13 @@
+package com.tolean.javatests;
+
+import com.tolean.javatests.completablefuture.CompletableFutureLearning;
+
+public class Application {
+
+    public static void main(String[] args) {
+        CompletableFutureLearning completableFutureLearning = new CompletableFutureLearning();
+        completableFutureLearning.run();
+        // log1
+    }
+
+}
