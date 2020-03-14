649ca3eae7095a35377c36aa309234d614113225
Tomek <tomasz.kolodziej@poczta.pl>
2020-03-14 14:15:26 +0100

    init

---
+++ src/main/java/com/tolean/javatests/Application.java
@@ -0,0 +1,12 @@
+package com.tolean.javatests;
+
+import com.tolean.javatests.completablefuture.CompletableFutureLearning;
+
+public class Application {
+
+    public static void main(String[] args) {
+        CompletableFutureLearning completableFutureLearning = new CompletableFutureLearning();
+        completableFutureLearning.run();
+    }
+
+}
---
+++ src/main/java/com/tolean/javatests/completablefuture/CompletableFutureLearning.java
@@ -0,0 +1,9 @@
+package com.tolean.javatests.completablefuture;
+
+public class CompletableFutureLearning {
+
+    public void run() {
+        System.out.println("completable future learning");
+    }
+
+}
